fff225c87e488fb647f12703dc5cc0adf50d45f7
Luis Manuel Vera <67924308+whitenael@users.noreply.github.com>
2022-04-26 14:42:20 -0300

    test_ultrasonido_implementado

--- molinos/test_ultrasonido/test_ultrasonido.ino
+++ molinos/test_ultrasonido/test_ultrasonido.ino
@@ -4,9 +4,9 @@ unsigned long seconds = 1000L; //unsigned = solo almacena numeros positivos
 unsigned long minutes = seconds * 60;
 unsigned long hours = minutes * 60;
 
-const float desc_alras = 30; // variable a confirmar
-const float alto_tanque = 100;
-const float diam_tanque = 560;
+const float desc_alras = 50; // variable a confirmar
+const float alto_tanque = 200;
+const float diam_tanque = 440;
 
 float vol_cm3;
 float vol_lit;
@@ -22,7 +22,6 @@ NewPing sonar(PinTrig, PinEcho);
 
 float distancia;
 int microSeconds;
-int eeAdress = 0;
 
 void setup()
 {
@@ -40,27 +39,10 @@ void loop()
   distancia = sonar.convert_cm(microSeconds);
   distanciaChequeada = chequearMedida(distancia);
 
-  if (distanciaChequeada == -1){
-    llenado = -1;
-  }
+  if (distanciaChequeada == -1){llenado=-1;}
 
-  else {
-    
-  // CALCULO DE VOLUMEN
+  else{llenado = calcularLlenado(distanciaChequeada);}
 
-  vol_cm3 = pi * (diam_tanque * diam_tanque) / 4 * alto_tanque;
-  vol_lit  = vol_cm3 / 1000;
-  auxiliar = vol_lit; //aux = 3141,6 Litros
-  llenado = 100.00;
-
-  // calcular el llenado real
-
-  vol_cm3 = pi * (diam_tanque * diam_tanque) / 4 * (alto_tanque - (distancia - desc_alras));
-  vol_lit  = vol_cm3 / 1000;         
-  llenado = vol_lit * 100.00 / auxiliar; 
-
-  }
-   
   Serial.println("########## RESULTADOS: ##########");
   Serial.print(llenado);
   Serial.print("%");
@@ -79,12 +61,11 @@ float chequearMedida(float medida){
   Serial.println(medida);
   
   int counter = 0;
+  int datoNuevo = medida;
 
-  if (medida < 30)
-  {
-    int datoNuevo = medida;
-       
-    while ((counter < 10) & (datoNuevo < desc_alras))
+  if (datoNuevo < desc_alras | datoNuevo > (alto_tanque + desc_alras))
+  {  
+    while ((counter < 10) & (datoNuevo < desc_alras | datoNuevo > (alto_tanque + desc_alras)))
     {
       Serial.print("Retomando muestra intento #");
       Serial.print(counter);
@@ -97,12 +78,32 @@ float chequearMedida(float medida){
       delay(200);
     }
 
-    if (counter <= 10){return -1;}
-    else{return datoNuevo;}
+    if (counter == 10){return -1;}
+    else{
+      Serial.print("Dato recolectado: ");
+      Serial.println(datoNuevo);
+      return datoNuevo;}
   }
 
   else {
     return medida;
   } 
   
+}
+
+float calcularLlenado(float distanciaChequeada){
+  // CALCULO DE VOLUMEN
+
+  vol_cm3 = pi * (diam_tanque * diam_tanque) / 4 * alto_tanque;
+  vol_lit  = vol_cm3 / 1000;
+  auxiliar = vol_lit; //aux = 3141,6 Litros
+  llenado = 100.00;
+
+  // calcular el llenado real
+
+  vol_cm3 = pi * (diam_tanque * diam_tanque) / 4 * (alto_tanque - (distanciaChequeada - desc_alras));
+  vol_lit  = vol_cm3 / 1000;         
+  llenado = vol_lit * 100.00 / auxiliar; 
+
+  return llenado;
 }
